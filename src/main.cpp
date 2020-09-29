995cf011500e0249e5c01ce64a55aed2d3755ea8
Maikol Guzman Alan <mguzmana@akamai.com>
2020-09-28 23:03:56 -0600

    Ejemplo serializar
    Signed-off-by: Maikol Guzman Alan <mguzmana@akamai.com>

--- src/main.cpp
+++ src/main.cpp
@@ -57,19 +57,21 @@ int main() {
 
     Persona persona1 = {"Ned Flanders", 18, 60};
 
-    // Convertir a JSON: Copia cada valor en un objeto JSON
+    // Convertir a JSON: Copia cada valor en un objeto JSON (SERIALIZACION)
     json jsonPersona;
     jsonPersona["nombre"] = persona1.getNombre();
     jsonPersona["edad"] = persona1.getEdad();
     jsonPersona["id"] = persona1.getId();
 
-    // Convertir desde JSON: copia cada valor desde un objeto de JSON
+    string personaSerializado = jsonPersona.dump();
+
+    // Convertir desde JSON: copia cada valor desde un objeto de JSON (DESEREALIZAR)
     Persona persona2;
     persona2.setNombre(jsonPersona["nombre"]);
     persona2.setEdad(jsonPersona["edad"]);
     persona2.setId(jsonPersona["id"]);
 
-    // Convertir de un String de JSON
+    // Convertir de un String de JSON (DESEREALIZAR)
     string data = R"({
                         "nombre": "Ned Flanders",
                         "edad": 18,
@@ -87,6 +89,8 @@ int main() {
     std::cout << "Persona 1: " << persona1.nombre << std::endl;
     std::cout << "Persona 2: " << persona2.nombre << std::endl;
     std::cout << "Persona 3: " << persona3.nombre << std::endl;
+
+    std::cout << "Persona 1 - Serializado en JSON: " << personaSerializado << std::endl;
 
     return 0;
 }
