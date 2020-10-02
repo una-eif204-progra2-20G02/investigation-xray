8acabb9c76e8fbe18e7cd10df321da35c8e78577
MinorBA <68918550+MinorBA@users.noreply.github.com>
2020-10-02 11:47:18 -0600

    serializacion de una lista de personas

--- src/main.cpp
+++ src/main.cpp
@@ -44,7 +44,6 @@ struct Persona {
 };
 
 int main() {
-
     /*
      * Ejemplo de una serialización del objeto Persona
      * https://github.com/nlohmann/json
@@ -58,6 +57,8 @@ int main() {
      */
 
     Persona persona1 = {"Ned Flanders", 18, 60};
+  /*  Persona persona2 = {"pep", 22, 43};
+    Persona persona3 = {"lucas", 33, 12};*/
 
     // Convertir a JSON: Copia cada valor en un objeto JSON (SERIALIZACION)
     json jsonPersona;
@@ -65,13 +66,32 @@ int main() {
     jsonPersona["edad"] = persona1.getEdad();
     jsonPersona["id"] = persona1.getId();
 
-    string personaSerializado = jsonPersona.dump();
+    string personaSerializado = jsonPersona.dump(4);
+
+/*
+    //SERIALIZANDO UNA LISTA.
+
+ */
+/*agrega otro objeto (usando una lista inicializadora)
+ j [ " objeto " ] = {{ " moneda " , " USD " }, { " valor " , 42,99 }};*//*
+
+
+   json jsonPersonas;
+
+jsonPersonas["Personas"] = {{" Persona 1: ",persona1.getNombre(),persona1.getEdad(),persona1.getId()},
+                            {" Persona 2: ",persona2.getNombre(),persona2.getEdad(),persona2.getId()},
+                            {" Persona 3: ",persona3.getNombre(),persona3.getEdad(),persona3.getId()}};
+
+string personasSerializando = jsonPersonas.dump();
+
+*/
+
 
     // Convertir desde JSON: copia cada valor desde un objeto de JSON (DESEREALIZAR)
-    Persona persona2;
-    persona2.setNombre(jsonPersona["nombre"]);
-    persona2.setEdad(jsonPersona["edad"]);
-    persona2.setId(jsonPersona["id"]);
+    Persona persona22;
+    persona22.setNombre(jsonPersona["nombre"]);
+    persona22.setEdad(jsonPersona["edad"]);
+    persona22.setId(jsonPersona["id"]);
 
     // Convertir de un String de JSON (DESEREALIZAR)
     string data = R"({
@@ -81,18 +101,20 @@ int main() {
                     })";
 
     json jsonData = json::parse(data);
-    Persona persona3;
-    persona3.setNombre(jsonData["nombre"]);
-    persona3.setEdad(jsonData["edad"]);
-    persona3.setId(jsonData["id"]);
+    Persona persona33;
+    persona33.setNombre(jsonData["nombre"]);
+    persona33.setEdad(jsonData["edad"]);
+    persona33.setId(jsonData["id"]);
+
 
     //guarda el archivo
     ofstream archivo;
     try { archivo.open("ArchivoTexto.json", ios::binary); }
-    catch (std::ifstream::failure a) {
+    catch (ifstream::failure a) {
         cout << "no se pudo abrir el archivo";
         exit(1);
     }
+    //para la lista de personas poner:archivo << personasSerializando<< '\n';
     archivo << personaSerializado << '\n';
     archivo.close();
 
@@ -100,7 +122,7 @@ int main() {
     ifstream archivo1;
     string texto;
     try { archivo1.open("ArchivoTexto.json", ios::binary); }
-    catch (std::ifstream::failure a) {
+    catch (ifstream::failure a) {
         cout << "no se pudo abrir el archivo";
         exit(1);
     }
@@ -109,16 +131,8 @@ int main() {
         getline(archivo1, texto);
         cout << texto << endl;
     }
-    archivo.close();
+    archivo1.close();
 
-
-/*std::cout << "Hello, Universidad Nacional!" << std::endl;
-
-    std::cout << "Persona 1: " << persona1.nombre << std::endl;
-    std::cout << "Persona 2: " << persona2.nombre << std::endl;
-    std::cout << "Persona 3: " << persona3.nombre << std::endl;
-
-    std::cout << "Persona 1 - Serializado en JSON: " << personaSerializado << std::endl;*/
 
     return 0;
 }
